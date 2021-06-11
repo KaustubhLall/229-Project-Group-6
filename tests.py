efaee4c0c3a7bd373fef269db12534485b997603
Kaustubh Lall <klall@ucsd.edu>
2021-06-10 18:00:53 -0700

    refactored testing

---
+++ tests.py
@@ -0,0 +1,40 @@
+from utils import *
+
+
+def test_remove_nan():
+    df = pd.read_csv('preprocess_data.csv')
+    df = remove_nan(df)
+    assert isinstance(df, pd.DataFrame)
+    assert not df.isnull().values.any()
+
+
+def test_predict_sales():
+    predict_sales('PC', 'Action', 'Nintendo', 'JP', "RF", "models_and_encoder")
+
+
+def test_remove_outliers():
+    import pandas as pd
+    df = pd.read_csv('preprocess_data.csv')
+    prices = ['NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales']
+
+    for col in prices:
+        df = remove_outliers(col, df)
+
+    assert len(df.shape) == 2
+    assert not df.isnull().values.any()
+    # check if function works as expected
+    for col in prices:
+        assert not (all([not isclose(list(df[col])[x], 0) for x in range(len(df[col]))]))
+
+
+def test_encode_df():
+    df = pd.read_csv('preprocess_data.csv')
+    df = encode_df(df)
+    assert not df.isnull().values.any()
+    assert any(['encoding' in c for c in list(df.columns)])
+
+
+def test_sale_visualization():
+    df = pd.read_csv("preprocess_data.csv")
+    df.head()
+    dff = sale_visualization('Publisher', 'Global_Sales')
--- utils.py
+++ utils.py
@@ -133,47 +133,8 @@ def remove_nan(df):
     return df.dropna()
 
 
-def test_remove_nan():
-    df = pd.read_csv('preprocess_data.csv')
-    df = remove_nan(df)
-    assert isinstance(df, pd.DataFrame)
-    assert not df.isnull().values.any()
-
-
-def test_predict_sales():
-    predict_sales('PC', 'Action', 'Nintendo', 'JP', "RF", "models_and_encoder")
-
-
-def test_remove_outliers():
-    import pandas as pd
-    df = pd.read_csv('preprocess_data.csv')
-    prices = ['NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales']
-
-    for col in prices:
-        df = remove_outliers(col, df)
-
-    assert len(df.shape) == 2
-    assert not df.isnull().values.any()
-    # check if function works as expected
-    for col in prices:
-        assert not (all([not isclose(list(df[col])[x], 0) for x in range(len(df[col]))]))
-
-
-def test_encode_df():
-    df = pd.read_csv('preprocess_data.csv')
-    df = encode_df(df)
-    assert not df.isnull().values.any()
-    assert any(['encoding' in c for c in list(df.columns)])
-
-
-def test_sale_visualization():
-    df = pd.read_csv("preprocess_data.csv")
-    df.head()
-    dff = sale_visualization('Publisher', 'Global_Sales')
-
-
 def sale_visualization(category, sale_region):
-    '''
+    """
     This function takes the category and sale_region as inputs and returns a pandas DataFrame as output,
     the total amount
     of the category items from a specific sales_region will be stored and returned
@@ -183,7 +144,7 @@ def sale_visualization(category, sale_region):
     output:
     returns: dff
     types: Pandas.DataFrame
-    '''
+    """
     assert category in ['Publisher', 'Genre', 'Platform']
     assert sale_region in ['Global_Sales', 'NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales']
     assert isinstance(category, str)
@@ -209,3 +170,4 @@ def sale_visualization(category, sale_region):
     dff = pd.DataFrame(list(dic.items()), columns=[category, sale_region])
 
     return dff
+
